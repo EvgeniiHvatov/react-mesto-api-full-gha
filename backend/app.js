2a736fdb7b1d967acb70b401c040dff8e4db256c
Evgenii Hvatov <hvatovspb@yandex.ru>
2023-08-20 18:53:26 +0300

    add logger

--- backend/app.js
+++ backend/app.js
@@ -11,17 +11,19 @@ const {
   login,
 } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(requestLogger);
 app.post('/signup', validateSignUp, createUser);
 app.post('/signin', validateSignIn, login);
 app.use(auth);
 app.use(router);
+app.use(errorLogger);
 app.use(errors());
 
 app.use((err, req, res, next) => {
