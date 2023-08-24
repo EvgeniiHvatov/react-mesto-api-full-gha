6a69a0c90fc5694b2897a57783b41106dad6168e
Evgenii Hvatov <hvatovspb@yandex.ru>
2023-08-24 21:54:19 +0300

    add handlerCORS &helmet

---
+++ backend/middlewares/handlerCORS.js
@@ -0,0 +1,26 @@
+const allowedCors = [
+  'http://localhost:3000',
+  'http://hvatovspb.nomoredomainsicu.ru',
+  'https://hvatovspb.nomoredomainsicu.ru',
+];
+
+const handlerCORS = (request, response, next) => {
+  const { origin } = request.headers;
+  const { method } = request;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = request.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    response.header('Access-Control-Allow-Origin', origin);
+  }
+
+  if (method === 'OPTIONS') {
+    response.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    response.header('Access-Control-Allow-Headers', requestHeaders);
+    return response.end();
+  }
+
+  return next();
+};
+
+module.exports = handlerCORS;
