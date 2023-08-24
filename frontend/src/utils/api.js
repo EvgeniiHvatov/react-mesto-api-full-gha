ef0d798ac344ff02bd9111497082d063227a3fd2
Evgenii Hvatov <hvatovspb@yandex.ru>
2023-08-24 19:43:50 +0300

    change api at frontend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,39 @@ class Api {
   constructor({baseUrl, headers}) {
     this._baseUrl = baseUrl;
     this._headers = headers;
+    this._authHeaders = null;
   }
+
+  setAuthHeaders(token) {
+    console.log(token);
+    this._authHeaders = { ...this._headers, authorization: `Bearer ${token}` };
+  }
+  deleteAuthHeaders() {
+    this._authHeaders = null;
+  }
+
+  // registerUser(regData) {
+  //   return fetch(`${this._baseUrl}/signup`, {
+  //       method: "POST",
+  //       headers: this._headers,
+  //       body: JSON.stringify(regData),
+  //   }).then(this._checkAnswer);
+  // }
+
+  // loginUser(loginData) {
+  //   return fetch(`${this._baseUrl}/signin`, {
+  //       method: "POST",
+  //       headers: this._headers,
+  //       body: JSON.stringify(loginData),
+  //   }).then(this._checkAnswer);
+  // }
+
+  // checkToken(token) {
+  //   return fetch(`${this._baseUrl}/users/me`, {
+  //       method: "GET",
+  //       headers: this._authHeaders,
+  //   }).then(this._checkAnswer);
+  // }
 
   _checkAnswer(res) {
     if (res.ok) {
@@ -15,7 +47,7 @@ class Api {
   getUserInfo() {
     const requestUrl = this._baseUrl + `/users/me`;
     return fetch(requestUrl, {
-      headers: this._headers,
+      headers: this._authHeaders,
     })
     .then(this._checkAnswer);
   }
@@ -23,7 +55,7 @@ class Api {
   getInitialCards() {
     const requestUrl = this._baseUrl + '/cards';
     return fetch(requestUrl, {
-      headers: this._headers,
+      headers: this._authHeaders,
     })
     .then(this._checkAnswer);
   }
@@ -36,7 +68,7 @@ class Api {
     const requestUrl = this._baseUrl + `/users/me`;
     return fetch(requestUrl, {
       method: 'PATCH',
-      headers: this._headers,
+      headers: this._authHeaders,
       body: JSON.stringify(body),
     })
     .then(this._checkAnswer);
@@ -46,7 +78,7 @@ class Api {
     const requestUrl = this._baseUrl + '/cards';
     return fetch(requestUrl, {
       method: 'POST',
-      headers: this._headers,
+      headers: this._authHeaders,
       body: JSON.stringify(body),
     })
     .then(this._checkAnswer);
@@ -56,7 +88,7 @@ class Api {
     const requestUrl = this._baseUrl + `/cards/${cardId}`;
     return fetch(requestUrl, {
       method: 'DELETE',
-      headers: this._headers,
+      headers: this._authHeaders,
     })
     .then(this._checkAnswer);
   }
@@ -65,7 +97,7 @@ class Api {
     const requestUrl = this._baseUrl + `/cards/likes/${cardId}`;
     return fetch(requestUrl, {
       method: 'PUT',
-      headers: this._headers,
+      headers: this._authHeaders,
     })
     .then(this._checkAnswer);
   }
@@ -74,7 +106,7 @@ class Api {
     const requestUrl = this._baseUrl + `/cards/likes/${cardId}`;
     return fetch(requestUrl, {
       method: 'DELETE',
-      headers: this._headers,
+      headers: this._authHeaders,
     })
     .then(this._checkAnswer);
   }
@@ -83,7 +115,7 @@ class Api {
     const requestUrl = this._baseUrl + `/users/me/avatar`;
     return fetch(requestUrl, {
       method: 'PATCH',
-      headers: this._headers,
+      headers: this._authHeaders,
       body: JSON.stringify(body),
     })
     .then(this._checkAnswer);
@@ -91,10 +123,9 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-66',
+  baseUrl: 'https://api.hvatovspb.nomoredomainsicu.ru',
   headers: {
-    authorization: 'd7dcd0f2-c4b8-4c41-b858-1f8610d52fd1',
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
   }
 });
 
